ad1ee0ed11f2d218817c8e3d3dbcfb4dccb9cdf3
HossainRobin <hellorobinhossain@gmail.com>
2023-09-16 23:11:47 +0600

    appbar with listTile widget added

---
+++ android/app/src/main/kotlin/com/example/module_5_exam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_5_exam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Shopping List"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.badge),
+              title: Text("Computer"),
+            ),
+            ListTile(
+              leading: Icon(Icons.badge),
+              title: Text("Laptop"),
+            ),
+            ListTile(
+              leading: Icon(Icons.badge),
+              title: Text("Fridge"),
+            ),
+            ListTile(
+              leading: Icon(Icons.badge),
+              title: Text("Monitor"),
+            ),
+            ListTile(
+              leading: Icon(Icons.badge),
+              title: Text("Fan"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
